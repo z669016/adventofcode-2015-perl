b57712aafa29b8e20f9efce2c07b6487c95315b0
Rene van Putten <z669016@gmail.com>
2022-07-21 06:48:24 +0200

    Changed constant to Readonly, which allows them to be used as key into a hash

--- aoc_input.pm
+++ aoc_input.pm
@@ -1,4 +1,5 @@
 package AOC::Input;
+
 use strict;
 use warnings FATAL => 'all';
 use feature qw/fc/;
@@ -8,13 +9,16 @@ our $VERSION = version->declare('v0.0.1');
 
 use File::Basename;
 use Text::CSV;
+use Readonly;
 
-use constant {
-    TXT   => ".txt",
-    CSV   => ".csv",
-};
+Readonly my $TXT => ".txt";
+Readonly my $CSV => ".csv";
+Readonly my $REG => ".reg";
+Readonly my @SUPPORTED_TYPES => ($TXT, $CSV, $REG);
 
-use constant SUPPORTED_TYPES => (TXT,CSV);
+Readonly my $SLICE => "slice";
+Readonly my $REGEXP => "regexp";
+Readonly my $SEP_CHAR => "sep_char";
 
 sub must_exist {
     my $path = shift;
@@ -23,18 +27,23 @@ sub must_exist {
 
 sub cmp_ext {
     my ($path, $expected) = @_;
-    my (undef, undef, $ext) = fileparse($path, SUPPORTED_TYPES);
+    my (undef, undef, $ext) = fileparse($path, @SUPPORTED_TYPES);
     fc($ext) eq fc($expected);
 }
 
 sub is_csv {
     my $path = shift;
-    cmp_ext($path, ".csv");
+    cmp_ext($path, $CSV);
 }
 
 sub is_txt {
     my $path = shift;
-    cmp_ext($path, ".txt");
+    cmp_ext($path, $TXT);
+}
+
+sub is_reg {
+    my $path = shift;
+    cmp_ext($path, $REG);
 }
 
 sub load_text {
@@ -46,18 +55,36 @@ sub load_text {
 sub load_csv {
     my ($fh, $options) = @_;
     my @data = ();
-    my $sep_char = $$options{'sep_char'} // ',';
-    my $csv = Text::CSV->new({ sep_char => $sep_char });
+    my $sep_char = $$options{$SEP_CHAR} // ',';
+    my $csv = Text::CSV->new({ $SEP_CHAR => $sep_char });
     while (<$fh>) {
         chomp;
 
         if ($csv->parse($_)) {
-            push @data, [$csv->fields()];
-        } else {
+            push @data, [ $csv->fields() ];
+        }
+        else {
             warn "Line could not be parsed: $_\n";
         }
     }
     return \@data;
+}
+
+sub load_reg {
+    my ($fh, $options) = @_;
+
+    unless (defined $$options{$REGEXP}) {
+        die "Missing option{regexp} for loading parseable text file.";
+    }
+
+    my $data = load_text($fh);
+    my $regexp = $$options{$REGEXP};
+    foreach (@$data) {
+        my @line = $_ =~ /$regexp/;
+        $_ = \@line;
+    }
+
+    return $data;
 }
 
 sub load {
@@ -70,18 +97,24 @@ sub load {
     my $data;
     if (is_txt($path)) {
         $data = load_text($fh);
-    } elsif (is_csv($path)) {
+    }
+    elsif (is_csv($path)) {
         $data = load_csv($fh, $options);
-    } else {
+    }
+    elsif (is_reg($path)) {
+        $data = load_reg($fh, $options);
+    }
+    else {
         die "Unknown input file type for '$path'";
     }
     close $fh;
 
-    my $slice = $$options{"slice"};
+    my $slice = $$options{$SLICE};
     if (defined $slice) {
         if (@$slice == 1) {
             return \(@$data)[@$slice];
-        } else {
+        }
+        else {
             return [ (@$data)[@$slice] ];
         }
     }
